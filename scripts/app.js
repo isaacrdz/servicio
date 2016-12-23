6859bae4be2a9aa63a0eb94d26ae8e1892220115
isaacrdz <zack_cpp@hotmail.com>
2016-12-22 18:59:10 -0600

    some changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,47 @@
 angular.module("serviceApp",[])
-.controller('mainCtrl', function($scope){
-	$scope.holla = function(){
-		console.log("Hola mundo");
+.controller('mainCtrl', function($scope, dataService){
+
+	$scope.helloConsole = dataService.helloConsole;
+
+	$scope.learningNgChange = function(){
+		console.log("An input is changed");
 	};
 	
 	$scope.customers = [
 		{
 			"name":"Isaac Alejandro Rodriguez Morales",
 			"email": "zack_cpp@hotmail.com",
+			"type": "Afinacion",
 			"status": "Pendiente"
 		},
 		{
 			"name":"Isaac Alejandro Rodriguez Morales",
 			"email": "zack_cpp@hotmail.com",
+			"type": "Afinacion",
 			"status": "Pendiente"
 		},
 		{
 			"name":"Isaac Alejandro Rodriguez Morales",
 			"email": "zack_cpp@hotmail.com",
+			"type": "Afinacion",
 			"status": "Pendiente"
 		},
 		{
 			"name":"Isaac Alejandro Rodriguez Morales",
 			"email": "zack_cpp@hotmail.com",
+			"type": "Afinacion",
 			"status": "Pendiente"
 		},
 		{
 			"name":"Isaac Alejandro Rodriguez Morales",
 			"email": "zack_cpp@hotmail.com",
+			"type": "Afinacion",
 			"status": "Pendiente"
 		}
 	]
+})
+.service('dataService', function(){
+	this.helloConsole = function(){
+		console.log('This is the hello console service');
+	}
 });

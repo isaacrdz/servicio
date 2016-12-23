afd08746a19fd57981fe1cbb64c0de96b60f0f73
isaacrdz <zack_cpp@hotmail.com>
2016-12-22 19:39:37 -0600

    get

--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,26 @@
 angular.module("serviceApp",[])
 .controller('mainCtrl', function($scope, dataService){
 
-	$scope.helloConsole = dataService.helloConsole;
 
 	$scope.learningNgChange = function(){
 		console.log("An input is changed");
 	};
 	
-	$scope.customers = [
-		{
-			"name":"Isaac Alejandro Rodriguez Morales",
-			"email": "zack_cpp@hotmail.com",
-			"type": "Afinacion",
-			"status": "Pendiente"
-		},
-		{
-			"name":"Isaac Alejandro Rodriguez Morales",
-			"email": "zack_cpp@hotmail.com",
-			"type": "Afinacion",
-			"status": "Pendiente"
-		},
-		{
-			"name":"Isaac Alejandro Rodriguez Morales",
-			"email": "zack_cpp@hotmail.com",
-			"type": "Afinacion",
-			"status": "Pendiente"
-		},
-		{
-			"name":"Isaac Alejandro Rodriguez Morales",
-			"email": "zack_cpp@hotmail.com",
-			"type": "Afinacion",
-			"status": "Pendiente"
-		},
-		{
-			"name":"Isaac Alejandro Rodriguez Morales",
-			"email": "zack_cpp@hotmail.com",
-			"type": "Afinacion",
-			"status": "Pendiente"
-		}
-	]
+	$scope.helloConsole = dataService.helloConsole;
+
+	dataService.getCustomers(function(response){
+			console.log(response.data);
+			$scope.customers = response.data;
+		});
+	
 })
-.service('dataService', function(){
+.service('dataService', function($http){
 	this.helloConsole = function(){
 		console.log('This is the hello console service');
-	}
+	};
+
+	this.getCustomers = function(callback) {
+		$http.get('mock/customers.json')
+		.then(callback)
+	} 
 });
